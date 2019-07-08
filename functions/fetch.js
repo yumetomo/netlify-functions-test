583af640c4c7abcee05c1cf9f639ae992aea327a
Yumetomo <yumetomo.eto@imjp.co.jp>
2019-07-08 21:18:00 +0900

    [add] netlify-lambda

---
+++ functions/fetch.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+
+exports.handler = async () => {
+  const URL = `https://xivapi.com/Town?key=${process.env.API_KEY}`;
+  return fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      try {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(data.Results)
+        };
+      } catch (error) {
+        return {
+          statusCode: 400,
+          body: error
+        };
+      }
+    })
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
