09fa623430c664d8f998312363cc6ba694626f0b
Yumetomo <yumetomo.eto@imjp.co.jp>
2019-07-08 20:01:08 +0900

    [add] node-fetch

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = require("node-fetch").default;
 exports.handler = async () => {
   const URL = 'https://xivapi.com/Recipe/1?language=ja'
   fetch(URL)
