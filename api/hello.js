94c7fe16ffb36b250eebe9001a536c3b936c29eb
Yumetomo <yumetomo.eto@imjp.co.jp>
2019-07-08 20:26:29 +0900

    [add] node-fetch

--- api/hello.js
+++ api/hello.js
@@ -1,13 +1,21 @@
 const fetch = require("node-fetch").default;
 
 exports.handler = async () => {
-  const URL = 'https://xivapi.com/Recipe/1?language=ja?key=${process.env.API_KEY}'
-  fetch(URL)
+  const URL = `https://xivapi.com/Town?key=${process.env.API_KEY}`;
+  return fetch(URL)
     .then(response => response.json())
     .then(data => {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data.Results)
-      };
+      try {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(data.Results)
+        };
+      } catch (error) {
+        return {
+          statusCode: 400,
+          body: error
+        };
+      }
     })
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 };
