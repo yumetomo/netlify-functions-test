344bfe2e4e880f978aae706321f0254b75f9c2f2
Yumetomo <yumetomo.eto@imjp.co.jp>
2019-07-10 12:00:26 +0900

    file add

--- resources/api/hello.js
+++ resources/api/hello.js
@@ -1,6 +1,14 @@
+const axios = require('axios');
+
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: 'Hello World',
-  };
+  const URL = `https://xivapi.com/Town?key=${process.env.KEY}`;
+  return axios.get(URL)
+    .then(({ data: data }) => ({
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }))
+    .catch(e => ({
+      statusCode: 400,
+      body: e
+    }));
 };
